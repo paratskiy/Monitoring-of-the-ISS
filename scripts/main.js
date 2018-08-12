64a9f0cbc8e22434435586bf8d0ed563b10bb335
Bogdan Paratskiy <bog4242@gmail.com>
2018-08-12 12:10:36 +0300

    dynamic movement of the ISS without restarting the map

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,58 @@
 let locationText = document.querySelector('.locationText');
-let astrosList   = document.querySelector('.astrosList');
-let totalAmount  = document.querySelector('.totalAmount span');
-let presentTime  = document.querySelector('.date h4');
-let presentDate  = document.querySelector('.date span');
-let icon_iss     = './img/iss.png';
-let locationData;
-let astrosAmount;
+let astrosList = document.querySelector('.astrosList');
+let totalAmount = document.querySelector('.totalAmount span');
+let presentTime = document.querySelector('.date h4');
+let presentDate = document.querySelector('.date span');
 
-function getDate(){
+let icon_iss = './img/iss.png';
+
+let locationData = null;
+let astrosAmount = null;
+let marker = null;
+
+function getDate() {
+
     locationData = new XMLHttpRequest();
-
     locationData.open('GET', 'http://api.open-notify.org/iss-now.json', false);
-
     locationData.send();
-
     if (locationData.status != 200) {
-        alert(locationData.status + ': ' + locationData.statusText); 
+        alert(locationData.status + ': ' + locationData.statusText);
     }
-
     locationData = JSON.parse(locationData.response);
 
+
     astrosAmount = new XMLHttpRequest();
-
     astrosAmount.open('GET', 'http://api.open-notify.org/astros.json', false);
-
     astrosAmount.send();
-
     if (astrosAmount.status != 200) {
-        alert(astrosAmount.status + ': ' + astrosAmount.statusText); 
+        alert(astrosAmount.status + ': ' + astrosAmount.statusText);
     }
-
     astrosAmount = JSON.parse(astrosAmount.response);
+
 
     setTimeout(getDate, 5000);
 }
 
 function setLocationText() {
     locationText.innerHTML = `longitude: ${locationData.iss_position.latitude}, latitude: ${locationData.iss_position.longitude}`;
+
     setTimeout(setLocationText, 5000);
 }
 
 function setDate() {
     let time = new Date(locationData.timestamp * 1000);
-    let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    let days   = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    let year   = time.getFullYear();
-    let month  = months[time.getMonth()];
-    let day    = days[time.getDay()];
-    let date   = time.getDate();
-    let hour   = time.getHours();
-    let min    = `${time.getMinutes() < 10 ? '0' : ''}${time.getMinutes()}`;
-    
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let year = time.getFullYear();
+    let month = months[time.getMonth()];
+    let day = days[time.getDay()];
+    let date = time.getDate();
+    let hour = time.getHours();
+    let min = `${time.getMinutes() < 10 ? '0' : ''}${time.getMinutes()}`;
+
     presentTime.innerHTML = `Current UTC time: ${hour}:${min}`;
     presentDate.innerHTML = `${day}, ${date} ${month} ${year}`;
+
     setTimeout(setDate, 5000);
 }
 
@@ -60,7 +60,7 @@ function setAstros() {
 
     let amount = []
     astrosList.innerHTML = '';
-    for(let i = 0; i < astrosAmount.people.length; i++) {
+    for (let i = 0; i < astrosAmount.people.length; i++) {
 
         let astro = astrosAmount.people[i];
 
@@ -81,33 +81,38 @@ function setAstros() {
             amount.push(astro);
         }
     }
+
     totalAmount.innerHTML = `Total amount: ${amount.length} people on ISS`
+
     setTimeout(setAstros, 5000);
 }
 
 function initMap() {
-    
-    var myLatLng = { lat: +locationData.iss_position.latitude, lng: +locationData.iss_position.longitude };
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    let myLatLng = { lat: +locationData.iss_position.latitude, lng: +locationData.iss_position.longitude };
+
+    let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 3,
         center: myLatLng
     });
 
-    var marker = new google.maps.Marker({
+    marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
         icon: icon_iss,
-        title: `longitude: ${locationData.iss_position.latitude}, latitude: ${locationData.iss_position.longitude}`
-    });
-    
-    setTimeout(initMap, 5000);
-}
+    })
+
+    function moveISS() {
+        marker.setPosition(new google.maps.LatLng(+locationData.iss_position.latitude, +locationData.iss_position.longitude));
+        setTimeout(moveISS, 5000);
+    }
+
+    moveISS();
+};
 
 getDate();
 setLocationText();
 setAstros();
-setDate()
-  
+setDate();
 
-  
+
